93d287e4e8537127fb6ca1d40197a71e48f157a0
Gloxyrwer <zlatorcev50819@mail.ru>
2023-09-15 17:53:27 +0700

    Поправил код в CameraController

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -11,24 +11,31 @@ public class CameraController : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         cam = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
         cameraHalfWidth = cam.aspect * cam.orthographicSize;
     }
 
     private void LateUpdate()
     {
-        float playerX = player.position.x;
-        float cameraLeftBoundary = transform.position.x - cameraHalfWidth;
-        float cameraRightBoundary = transform.position.x + cameraHalfWidth;
+        if (player.position.x != transform.position.x)
+        {
+            float playerX = player.position.x;
+            float cameraLeftBoundary = transform.position.x - cameraHalfWidth;
+            float cameraRightBoundary = transform.position.x + cameraHalfWidth;
 
-        if (playerX < cameraLeftBoundary)
-        {
-            offsetX = cameraLeftBoundary - cameraRightBoundary;
-            transform.Translate(new Vector3(offsetX, 0f, 0f));
-        }
-        else if (playerX > cameraRightBoundary)
-        {
-            offsetX = cameraRightBoundary - cameraLeftBoundary;
-            transform.Translate(new Vector3(offsetX, 0f, 0f));
+            if (playerX < cameraLeftBoundary)
+            {
+                offsetX = cameraLeftBoundary - cameraRightBoundary;
+                transform.Translate(new Vector3(offsetX, 0f, 0f));
+            }
+            else if (playerX > cameraRightBoundary)
+            {
+                offsetX = cameraRightBoundary - cameraLeftBoundary;
+                transform.Translate(new Vector3(offsetX, 0f, 0f));
+            }
         }
     }
 }
