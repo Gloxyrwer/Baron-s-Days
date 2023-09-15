99a744b134b82dacd72b780587681ed75a850ab1
Gloxyrwer <zlatorcev50819@mail.ru>
2023-09-15 16:53:53 +0700

    Переделал камеру и поменял коллайдер на игроке

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform player;
+    private Camera cam;
+    private float cameraHalfWidth;
 
-    private void Start()
+    private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        cam = GetComponent<Camera>();
+        cameraHalfWidth = cam.aspect * cam.orthographicSize;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
-        Vector3 temp = transform.position;
-        temp.x = player.position.x;
-        temp.y = transform.position.y;
-        temp.z = transform.position.z;
+        float playerX = player.position.x;
+        float cameraLeftBoundary = transform.position.x - cameraHalfWidth;
+        float cameraRightBoundary = transform.position.x + cameraHalfWidth;
 
-        transform.position = temp;
+        if (playerX < cameraLeftBoundary || playerX > cameraRightBoundary)
+        {
+            float offsetX = playerX - transform.position.x;
+
+            transform.Translate(new Vector3(offsetX, 0f, 0f));
+        }
     }
 }
