656046edb0defa53918db0bdc760ed65d80cbd4b
Gloxyrwer <zlatorcev50819@mail.ru>
2023-09-15 17:45:57 +0700

    Переделал камеру

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,6 +5,7 @@ public class CameraController : MonoBehaviour
     private Transform player;
     private Camera cam;
     private float cameraHalfWidth;
+    private float offsetX;
 
     private void Awake()
     {
@@ -19,10 +20,14 @@ public class CameraController : MonoBehaviour
         float cameraLeftBoundary = transform.position.x - cameraHalfWidth;
         float cameraRightBoundary = transform.position.x + cameraHalfWidth;
 
-        if (playerX < cameraLeftBoundary || playerX > cameraRightBoundary)
+        if (playerX < cameraLeftBoundary)
         {
-            float offsetX = playerX - transform.position.x;
-
+            offsetX = cameraLeftBoundary - cameraRightBoundary;
+            transform.Translate(new Vector3(offsetX, 0f, 0f));
+        }
+        else if (playerX > cameraRightBoundary)
+        {
+            offsetX = cameraRightBoundary - cameraLeftBoundary;
             transform.Translate(new Vector3(offsetX, 0f, 0f));
         }
     }
